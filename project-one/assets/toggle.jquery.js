4d0b34290f837f0e0d9645d02f8beb5376a8f1cd
Joriyan <jlee70@nd.edu>
2019-09-29 13:40:08 -0400

    css updates
    my button is not working

--- project-one/assets/toggle.jquery.js
+++ project-one/assets/toggle.jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function (){
     //ID or Class name of the button goes into the first section
-    $('#clickme').click(function() {
+    $('#button').click(function() {
         //ID or Class name of the modal you want to appear on click goes into this second area.
         $("#letter-analysis").toggleClass("visible");
         });
