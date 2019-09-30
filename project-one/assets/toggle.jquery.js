428f0ec1010afc2fe61fd082b99d82929aa9e339
Joriyan <jlee70@nd.edu>
2019-09-29 23:22:00 -0400

    project one touches

--- project-one/assets/toggle.jquery.js
+++ project-one/assets/toggle.jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function (){
     //ID or Class name of the button goes into the first section
-    $('#button').click(function() {
+    $('#clickme').click(function() {
         //ID or Class name of the modal you want to appear on click goes into this second area.
         $("#letter-analysis").toggleClass("visible");
         });
