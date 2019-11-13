3e5c14d49dba284807b188675802727160aa6a0e
Joriyan <jlee70@nd.edu>
2019-11-13 09:55:19 -0500

    background and quote color change

--- project-two/assets/fadein-out.jquery.js
+++ project-two/assets/fadein-out.jquery.js
@@ -1,109 +1,111 @@
-$(document).ready(function () {
-	//ID or Class name of the button goes into the first section
-	$('#blue-circle').click(function () {
-		//ID or Class name of the modal you want to appear on click goes into this second area.
-		$("#bluepaint").toggleClass("invisible");
-		$(".blue-opt2").toggleClass("invisible");
-		$("#yellow-circle").toggleClass("invisible");
-		$("#red-circle").toggleClass("invisible");
-		$("#blue-circle").toggleClass("active");
-		$("#yellow-green").removeClass("active");
-		$("#blue-green").removeClass("active");
-		$("#red-purple").removeClass("active");
-		$("#purplepaint").addClass("invisible");
-		$("#greenpaint").addClass("invisible");
-	});
+$(document).ready(function() {
+  //ID or Class name of the button goes into the first section
+  $('#blue-circle').click(function() {
+    //ID or Class name of the modal you want to appear on click goes into this second area.
+    $("#bluepaint").toggleClass("invisible");
+    $(".blue-opt2").toggleClass("invisible");
+    $("#blue-circle").toggleClass("active");
+
+    $("#yellow-circle").toggleClass("invisible");
+    $("#red-circle").toggleClass("invisible");
+
+    $("#blue-yellow").removeClass("active");
+    $("#blue-red").removeClass("active");
+    $("#purplepaint").addClass("invisible");
+    $("#greenpaint").addClass("invisible");
+  });
 
 
 
-	$('#red-circle').click(function () {
-		//ID or Class name of the modal you want to appear on click goes into this second area.
-		$("#redpaint").toggleClass("invisible");
-		$(".red-opt2").toggleClass("invisible");
-		$("#blue-circle").toggleClass("invisible");
-		$("#yellow-circle").toggleClass("invisible");
-		$("#red-circle").toggleClass("active");
-		$("#purplepaint").addClass("invisible");
-		$("#bluepaint").addClass("invisible");
-		$("#orangepaint").addClass("invisible");
-		$("#yellow-orange").removeClass("active");
-		$("#blue-purple").removeClass("active");
+	  $('#red-circle').click(function() {
+    //ID or Class name of the modal you want to appear on click goes into this second area.
+    $("#redpaint").toggleClass("invisible");
+    $(".red-opt2").toggleClass("invisible");
+    $("#red-circle").toggleClass("active");
+
+    $("#blue-circle").toggleClass("invisible");
+    $("#yellow-circle").toggleClass("invisible");
+
+    $("#red-yellow").removeClass("active");
+    $("#red-blue").removeClass("active");
+    $("#purplepaint").addClass("invisible");
+    $("#orangepaint").addClass("invisible");
+    // $("#bluepaint").addClass("invisible");
+
+  });
+	
+ $('#yellow-circle').click(function() {
+    //ID or Class name of the modal you want to appear on click goes into this second area.
+    $("#yellowpaint").toggleClass("invisible");
+    $(".yellow-opt2").toggleClass("invisible");
+    $("#yellow-circle").toggleClass("active");
+
+    $("#blue-circle").toggleClass("invisible");
+    $("#red-circle").toggleClass("invisible");
+
+    $("#yellow-red").removeClass("active");
+    $("#yellow-blue").removeClass("active");
+    $("#greenpaint").addClass("invisible");
+    $("#orangepaint").addClass("invisible");
+    // $("#purplepaint").addClass("invisible");
+    // $("#bluepaint").addClass("invisible");
+	 
+  });
+	
+	 // the two colors that are turned on when the user clicks the BLUE circle as soon as they arrive on the site
+  $('#blue-red').click(function() {
+    $("#purplepaint").removeClass("invisible");
+    $("#blue-red").addClass("active");
+
+    $("#greenpaint").addClass("invisible");
+    $("#blue-yellow").removeClass("active");
+  });
+
+  $('#blue-yellow').click(function() {
+    $("#greenpaint").removeClass("invisible");
+    $("#blue-yellow").addClass("active");
+
+    $("#purplepaint").addClass("invisible");
+    $("#blue-red").removeClass("active");
+  });
+
+  // the two colors that are turned on when the user clicks the RED circle as soon as they arrive on the site
+  $('#red-blue').click(function() {
+    //ID or Class name of the modal you want to appear on click goes into this second area.
+    $("#purplepaint").removeClass("invisible");
+    $("#red-blue").addClass("active");
+
+    $("#orangepaint").addClass("invisible");
+    $("#red-yellow").removeClass("active");
+  });
+
+  $('#red-yellow').click(function() {
+    //ID or Class name of the modal you want to appear on click goes into this second area.
+    $("#orangepaint").removeClass("invisible");
+    $("#red-yellow").addClass("active");
+
+    $("#purplepaint").addClass("invisible");
+    $("#red-blue").removeClass("active");
+  });
 
 
 
-	});
-	$('#yellow-circle').click(function () {
-		//ID or Class name of the modal you want to appear on click goes into this second area.
-		$("#yellowpaint").toggleClass("invisible");
-		$(".yellow-opt2").toggleClass("invisible");
-		$("#blue-circle").toggleClass("invisible");
-		$("#red-circle").toggleClass("invisible");
-		$("#yellow-circle").toggleClass("active");
-		$("#red-purple").removeClass("active");
-		$("#purplepaint").addClass("invisible");
-		$("#bluepaint").addClass("invisible");
-		$("#greenpaint").addClass("invisible");
-		$("#yellow-green").removeClass("active");
+// the two colors that are turned on when the user clicks the YELLOW circle as soon as they arrive on the site
+  $('#yellow-red').click(function() {
 
+    $("#orangepaint").removeClass("invisible");
+    $("#yellow-red").addClass("active");
 
-	});
-	$('#red-purple').click(function () {
-		//ID or Class name of the modal you want to appear on click goes into this second area.
-		$("#purplepaint").removeClass("invisible");
-		$("#red-circle").toggleClass("active");
-		$("#red-purple").addClass("active");
-		$("#yellow-green").removeClass("active");
-		$("#greenpaint").addClass("invisible");
-		$("#bluepaint").addClass("invisible");
+    $("#greenpaint").addClass("invisible");
+    $("#yellow-blue").removeClass("active");
+  });
 
-	});
-	$('#blue-purple').click(function () {
-		//ID or Class name of the modal you want to appear on click goes into this second area.
-		$("#purplepaint").removeClass("invisible");
-		$("#blue-purple").addClass("active");
-		$("#blue-circle").toggleClass("active");
-		$("#yellow-orange").removeClass("active");
-		$("#orangepaint").addClass("invisible");
-		$("#greenpaint").addClass("invisible");
-		$("#yellow-circle").addClass("active");
-		
+  $('#yellow-blue').click(function() {
+    
+    $("#greenpaint").removeClass("invisible");
+    $("#yellow-blue").addClass("active");
 
-	});
-	$('#red-orange').click(function () {
-		//ID or Class name of the modal you want to appear on click goes into this second area.
-		$("#orangepaint").removeClass("invisible");
-		$("#yellow-orange").addClass("active");
-		$("#greenpaint").addClass("invisible");
-		$("#yellow-circle").toggleClass("active");
-		$("#red-orange").addClass("active");
-		$("#greenpaint").addClass("invisible");
-		$("#bluepaint").addClass("invisible");
-	});
-	$('#yellow-orange').click(function () {
-		//ID or Class name of the modal you want to appear on click goes into this second area.
-		$("#orangepaint").removeClass("invisible");
-		$("#yellow-orange").addClass("active");
-		$("#red-orange").addClass("active");
-		$("#red-purple").removeClass("active");
-		$("#purplepaint").addClass("invisible");
-		$("#greenpaint").addClass("invisible");
-		$("#bluepaint").addClass("invisible");
-	});
-	$('#yellow-green').click(function () {
-		//ID or Class name of the modal you want to appear on click goes into this second area.
-		$("#greenpaint").removeClass("invisible");
-		$("#red-purple").addClass("active");
-		$("#yellow-green").removeClass("active");
-		$("#purplepaint").addClass("invisible");
-		$("#orangepaint").addClass("invisible");
-		$("#bluepaint").removeClass("invisible");
-	});
-	$('#blue-green').click(function () {
-		//ID or Class name of the modal you want to appear on click goes into this second area.
-		$("#greenpaint").removeClass("invisible");
-		$("#red-purple").removeClass("active");
-		$("#purplepaint").addClass("invisible");
-		$("#orangepaint").addClass("invisible");
-		$("#bluepaint").removeClass("invisible");
-	});
+    $("#orangepaint").addClass("invisible");
+    $("#yellow-red").removeClass("active");
+  });
 });
